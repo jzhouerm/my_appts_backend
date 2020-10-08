f1975923e3f0c224819b8ae4fab355af57c55bc9
“jzhouerm” <jzhouerm@hotmail.com>
2020-10-08 15:20:05 -0400

    services & users controllers

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,52 @@
 class ServicesController < ApplicationController
+    def index
+        services = Service.all
+        render json: ServiceSerializer.new(services, options).serialized_json
+    end
+
+    def show
+        service = Service.find_by(id: params[:id])
+        render json: ServiceSerializer.new(service, options).serialized_json
+    end
+
+    def create 
+        service = Service.new(service_params)
+        if service.save 
+            render json: ServiceSerializer.new(service).serialized_json
+        else
+            render json: { error: service.errors.messages }, status: 422     #unprocessable entity error
+        end
+
+    end
+
+    def update 
+        service = Service.find_by(id: params[:id])
+        if service.update(service_params)
+            render json: ServiceSerializer.new(service, options).serialized_json
+        else
+            render json: { error: service.errors.messages }, status: 422
+        end
+        
+    end
+
+    def destroy
+        service = Service.find_by(id: params[:id])
+        if service.destroy
+            head :no_content
+        else
+            render json: { error: service.errors.messages }, status: 422 
+        end
+        
+    end
+
+    private
+
+    def service_params
+        params.require(:service).permit(:name, :description, :amount)
+    end
+
+#add options as 2nd argument for index, show, update methods to provide associated data
+    def options
+        @options ||= { include: %i[appointments] }
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,53 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: UserSerializer.new(users, options).serialized_json
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+        render json: UserSerializer.new(user, options).serialized_json
+    end
+
+    def create 
+        user = User.new(user_params)
+        if user.save 
+            render json: UserSerializer.new(user).serialized_json
+        else
+            render json: { error: user.errors.messages }, status: 422     #unprocessable entity error
+        end
+
+    end
+
+    def update 
+        user = User.find_by(id: params[:id])
+        if user.update(user_params)
+            render json: UserSerializer.new(user, options).serialized_json
+        else
+            render json: { error: user.errors.messages }, status: 422
+        end
+        
+    end
+
+    def destroy
+        user = User.find_by(id: params[:id])
+        if user.destroy
+            head :no_content
+        else
+            render json: { error: user.errors.messages }, status: 422 
+        end
+        
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:business_name, :first_name, :last_name, :address, :email, :password)
+    end
+
+#add options as 2nd argument for index, show, update methods to provide associated data
+    def options
+        @options ||= { include: %i[appointments] }
+    end
 end
